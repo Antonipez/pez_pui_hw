78fcc5dfce7dc43883c9889d0082e8d90c016157
Antonipez <peihaoz@andrew.cmu.edu>
2022-11-30 14:56:40 -0500

    FP 11.30

--- Final_Project/app.js
+++ Final_Project/app.js
@@ -1,21 +1,21 @@
-function scrollHorizontally(e) {
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    var scrollSpeed = 15;
-    document.documentElement.scrollLeft -= (delta * scrollSpeed);
-    document.body.scrollLeft -= (delta * scrollSpeed);
-    e.preventDefault();
-  }
+// function scrollHorizontally(e) {
+//     e = window.event || e;
+//     var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+//     var scrollSpeed = 15;
+//     document.documentElement.scrollLeft -= (delta * scrollSpeed);
+//     document.body.scrollLeft -= (delta * scrollSpeed);
+//     e.preventDefault();
+//   }
   
-  if (window.addEventListener) {
-    // IE9, Chrome, Safari, Opera
-    window.addEventListener("mousewheel", scrollHorizontally, false);
-    // Firefox
-    window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
-  } else {
-    // IE 6/7/8
-    window.attachEvent("onmousewheel", scrollHorizontally);
-  }
+//   if (window.addEventListener) {
+//     // IE9, Chrome, Safari, Opera
+//     window.addEventListener("mousewheel", scrollHorizontally, false);
+//     // Firefox
+//     window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+//   } else {
+//     // IE 6/7/8
+//     window.attachEvent("onmousewheel", scrollHorizontally);
+//   }
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -31,3 +31,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 });
 
+
+import 'animate.css';
+
+import { gsap } from "gsap";
+
+
+
+document.querySelectorAll(".act-text").forEach((dot, i) => {  
+  gsap.to(img, { duration: 2.5, ease: "power4.out", y: -500 });
+});

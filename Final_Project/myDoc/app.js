f340f8d0df8ec123c9fdb2dd66994d7b684c7100
Antonipez <peihaoz@andrew.cmu.edu>
2022-12-07 22:02:46 -0500

    FP 12.7

--- Final_Project/myDoc/app.js
+++ Final_Project/myDoc/app.js
@@ -1,26 +1,4 @@
-// function scrollHorizontally(e) {
-//     e = window.event || e;
-//     var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-//     var scrollSpeed = 15;
-//     document.documentElement.scrollLeft -= (delta * scrollSpeed);
-//     document.body.scrollLeft -= (delta * scrollSpeed);
-//     e.preventDefault();
-//   }
-  
-//   if (window.addEventListener) {
-//     // IE9, Chrome, Safari, Opera
-//     window.addEventListener("mousewheel", scrollHorizontally, false);
-//     // Firefox
-//     window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
-//   } else {
-//     // IE 6/7/8
-//     window.attachEvent("onmousewheel", scrollHorizontally);
-//   }
-
-
-// import 'animate.css';
-
-
+// vanilla js library, progress bar when scroll for landing-page and axo-view
 document.addEventListener("DOMContentLoaded", function (event) {
     progressJS.start({
         "color": "#ed1765",
@@ -35,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 
+// jQuery library, rainbow star cursor for acts
 jQuery('.all-acts').jstars({
 	image_path: '../jQuery-Plugin-For-Magic-Cursor-Animations-jStars/images/', // folder with magic image
 	style: 'rand',       // optional, color, default: white
@@ -44,3 +23,23 @@ jQuery('.all-acts').jstars({
 	delay: 300 // rotate speed
 });
 
+
+// horizontal scroll when scrolling vertically for acts
+function scrollHorizontally(e) {
+  e = window.event || e;
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  var scrollSpeed = 15;
+  document.documentElement.scrollLeft -= (delta * scrollSpeed);
+  document.body.scrollLeft -= (delta * scrollSpeed);
+  e.preventDefault();
+}
+
+if (window.addEventListener) {
+  // IE9, Chrome, Safari, Opera
+  window.addEventListener("mousewheel", scrollHorizontally, false);
+  // Firefox
+  window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+  // IE 6/7/8
+  window.attachEvent("onmousewheel", scrollHorizontally);
+}
